3da97403ad99f892d028f1c8bad7a72178b5da3d
OLEG <schastliviy.sluchay@ukr.net>
2021-11-25 10:24:11 +0200

    0.0

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import styles from "./Header.module.css";
+
+export default function Header() {
+  return (
+    <header>
+      <nav className={styles.nav}>
+        <Link to="/" className={styles.header__nav_links}>
+          Home
+        </Link>
+        <Link to className={styles.header__nav_links}>
+          Movies
+        </Link>
+      </nav>
+    </header>
+  );
+}
