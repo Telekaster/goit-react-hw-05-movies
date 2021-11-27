0462114e480e5f26c3258afc8f94e351aade6af0
OLEG <schastliviy.sluchay@ukr.net>
2021-11-27 14:47:22 +0200

    0.7

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 https://telek-goit-react-hw-05-movies-telek.netlify.app/
+
+0.7
--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,7 @@
+const BASE_URL = "api.themoviedb.org/3";
+const key = "61165aac189ece3ae64e67d82e58db65";
+
 export async function fetchSearch(keyword) {
-  const BASE_URL = "api.themoviedb.org/3";
-  const key = "61165aac189ece3ae64e67d82e58db65";
   const result = await fetch(
     `https://${BASE_URL}/search/movie/?query=${keyword}&api_key=${key}&language=en-US&page=1&include_adult=false`
   )
@@ -14,8 +15,6 @@ export async function fetchSearch(keyword) {
 }
 
 export async function fetchMovies(typeOfRequest) {
-  const BASE_URL = "api.themoviedb.org/3";
-  const key = "61165aac189ece3ae64e67d82e58db65";
   const result = await fetch(
     `https://${BASE_URL}/${typeOfRequest.current}?api_key=${key}`
   )
@@ -29,8 +28,6 @@ export async function fetchMovies(typeOfRequest) {
 }
 
 export async function fetchReviews(movie_id) {
-  const BASE_URL = "api.themoviedb.org/3";
-  const key = "61165aac189ece3ae64e67d82e58db65";
   const result = await fetch(
     `https://${BASE_URL}/movie/${movie_id}/reviews?api_key=${key}&language=en-US&page=1`
   )
@@ -44,8 +41,6 @@ export async function fetchReviews(movie_id) {
 }
 
 export async function fetchCredits(movie_id) {
-  const BASE_URL = "api.themoviedb.org/3";
-  const key = "61165aac189ece3ae64e67d82e58db65";
   const result = await fetch(
     `https://${BASE_URL}/movie/${movie_id}/credits?api_key=${key}&language=en-US`
   )
